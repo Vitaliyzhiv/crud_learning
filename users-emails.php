908a1224e69379b033702a98b6a624fe182c3f0e
vitaliyzhiv <zhmud12@gmail.com>
2024-08-25 04:33:58 +0300

    add edit for user emails table

--- users-emails.php
+++ users-emails.php
@@ -56,11 +56,105 @@ $emails_data = $result->findAll();
                             <td><?php echo htmlspecialchars($email['user']); ?></td>
                             <td><?php echo htmlspecialchars($email['email']); ?></td>
                             <td>
-                                <a href="?id=<?php echo $email['id'];?>" class="btn btn-primary">Edit</a>
+                            <a href="?id=<?php echo $email['id']; ?>" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#edit<?php echo $email['id']; ?>">
+                                Edit
+                            </a>
                                 <!-- Delete button -->
                                 <a href="?id=<?php echo $email['id'];?>" class="btn btn-danger delete-emails-btn" data-id="<?php echo $email['id'];?>" name="delete">Delete</a>
                             </td>
                         </tr>
+
+                         <!-- Modal Edit -->
+                    <div class="modal fade" id="edit<?php echo $email['id']; ?>" tabindex="-1" role="dialog"
+                        aria-labelledby="editModalLabel<?php echo $email['id']; ?>" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="editModalLabel<?php echo $email['id']; ?>">Изменить
+                                        запись</h5>
+                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form id="editForm<?php echo $email['id']; ?>">
+                                        <!-- No form tag here to prevent default submission -->
+                                        <div class="form-group">
+                                            <small>Имя</small>
+                                            <input type="text" class="form-control" id="name<?php echo 
+                                            $email['id']; ?>"
+                                                value="<?php echo htmlspecialchars($email['user']); ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <small>Почта</small>
+                                            <input type="text" class="form-control"
+                                                id="email<?php echo $email['id']; ?>"
+                                                value="<?php echo htmlspecialchars($email['email']); ?>">
+                                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Закрыть</button>
+                                    <button type="button" class="btn btn-primary"
+                                        onclick="submitForm(<?php echo $email['id']; ?>)">Редактировать</button>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+
+                        <script>
+                            function submitForm(id) {
+                                var name = $('#name' + id).val();
+                                var email = $('#email' + id).val();
+
+                                var data = {
+                                    'user': name,
+                                    'email': email
+                                };
+
+                                $.ajax({
+                                    url: 'form.php',
+                                    type: 'POST',
+                                    data: {
+                                        id: id,
+                                        table: 'data_users',
+                                        dataTable: JSON.stringify(data),
+                                        edit: true
+                                    },
+                                    dataType: 'json',
+                                    success: function (response) {
+                                        console.log(response);
+                                        if (response.success) {
+                                            $('#name' + id).val(name);
+                                            $('#email' + id).val(email);
+
+
+                                            $('tr').each(function () {
+                                                var rowId = $(this).find('td:first').text();
+                                                if (rowId == id) {
+                                                    $(this).find('td:nth-child(2)').text(name);
+                                                    $(this).find('td:nth-child(3)').text(email);
+                                                }
+                                            });
+
+                                            $('#edit' + id).modal('hide');
+
+                                            alert(response.message);
+                                        } else {
+                                            alert('Failed to update item');
+                                        }
+                                    },
+                                    error: function (xhr, status, error) {
+                                        console.log(xhr.responseText);
+                                        alert('An error occurred');
+                                    }
+                                });
+                            }
+                        </script>
+
+
+                    </div>
                     <?php endforeach; ?>   
                 </tbody>
             </table>
