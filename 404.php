483036c1f94162e777197f06b079632d1c169e54
vitaliyzhiv <zhmud12@gmail.com>
2024-09-05 03:27:22 +0300

    add 404 page to router

--- 404.php
+++ 404.php
@@ -1,9 +1,18 @@
 <?php 
-echo 'Страница не найдена';
+use App\Templates\Header;
+use App\Templates\Footer;
+
+$header = new Header();
+$header->render();
 ?>
 
 <div class="container mt-5">
-<p class="errors"></p>
     <!--  return to main page button -->
-    <a href="http://localhost/" class="btn btn-outline-primary w-100">Back to main page</a>
+    <a href="http://localhost/" class="btn btn-outline-primary w-100 mb-5">Back to main page</a>
+    <p class="errors h1 text-center">
+        <strong> 404 Page not found</strong>
+    </p>
 </div>
+
+<?php $footer = new Footer();
+$footer->render();
